eac18c5802337ea6b2329c021c951d32d0249a52
Abdul Kabia <akkabia@gmail.com>
2018-02-14 13:48:49 -0500

    Removed previous PDF parser, added new one

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ var Multer = require('multer');
 var upload = Multer({ dest: 'uploads/' });
 var fs = require('fs');
 var fileUpload = require('express-fileupload');
-var pdf = require('pdf-parse');
-
+var pdfText = require('pdf-text');
 
 parser.addRule(phoneReg, '');
 
@@ -22,6 +21,18 @@ parser.addRule(phoneReg, '');
   res.redirect('/api/phonenumbers/parse/text/')
 });*/
 //create a server object:
+
+async function readMyPDF(fileContents) {
+  var myReturn;
+  pdfText(fileContents, async function(err, chunks) {
+    myReturn = await chunks;
+    // console.log(chunks);
+  });
+
+  return await myReturn;
+  
+}
+
 
 app.get('/', function (req, res) {
   //res.status(400).send('[]');
@@ -42,7 +53,7 @@ app.get('/api/phonenumbers/parse/text/:pString', function (req, res) {
 
 });
 
-app.post('/api/phonenumbers/parse/file', upload.single('file'), function (req, res) {
+app.post('/api/phonenumbers/parse/file', upload.single('file'), async function (req, res) {
 
   if (!req.file) {
     res.status(400).send('File not found');
@@ -50,48 +61,52 @@ app.post('/api/phonenumbers/parse/file', upload.single('file'), function (req, r
 
   else {
     var inFile = req.file.path;
-    var datBuff = fs.readFileSync(inFile);
+    var fileContents = fs.readFileSync(inFile);
+    var fileText;
 
-    fs.readFile(req.file.path, function (err, contents) {
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
+    console.log(readMyPDF(fileContents));
+    res.status(200).send(myPDF);
+    // fs.readFile(req.file.path, function (err, contents) {
+    //   if (err) {
+    //     res.status(500).send(err);
+    //     return;
+    //   }
 
-      var fileText;
-      var numArr = [];
-      if (req.file.originalname.match(/.*.pdf/))  {
-        pdf(contents).then(data => {
-          fileText = data.text.toString('ascii');
-          var tempNumArr = fileText.split('\n');
-
-          numArr = tempNumArr.filter(function(x){
-            return (x !== (undefined || null || ''));
-          });
+    //   var fileText;
+    //   var numArr = [];
+    //   if (req.file.originalname.match(/.*.pdf/))  {
+    //     pdf(contents).then(data => {
+    //       fileText = data.text;
+    //       // var tempNumArr = fileText.split('\n');
+    //       // fileText = Buffer.from(fileText, 'base64');
+    //       // numArr = fileText.filter(function(x){
+    //       //   return (x !== (undefined || null || ''));
+    //       // });
+    //       base64.decode(fileText, function(err, base64String) {
+    //         console.log(base64String);
+    //       });
           
-        });
-      }
-      else {
-        fileText = contents.toString('ascii');
-        
-        var buf = Buffer.from(fileText, 'base64');
-        var numbers = buf.toString('ascii');
-        numArr = numbers.split('\n');
-        console.log(numArr);
-      }
+          
+    //     });
+    //   }
+    //   else {
+    //     var numbers = Buffer.from(contents, 'base64').toString('ascii');  
+    //     numArr = numbers.split('\n');
+    //   }
       
-      var phone = [];
-      for (var i = 0; i < numArr.length; i++) {
-        phone[i] = numArr[i];
-        phone[i] = phoneUtil.parse(numArr[i], 'CA');
-        phone[i] = phoneUtil.format(phone[i], PNF.INTERNATIONAL);
-      }
+    //   var phone = [];
+    //   for (var i = 0; i < numArr.length; i++) {
+    //     phone[i] = numArr[i];
+    //     phone[i] = phoneUtil.parse(numArr[i], 'CA');
+    //     phone[i] = phoneUtil.format(phone[i], PNF.INTERNATIONAL);
+    //   }
 
-      res.status(200).send(phone);
-    });
+    //   res.status(200).send(phone);
+    // });
    
   }
 });
+
 
 app.listen(port, (err) => {
   if (err) {
